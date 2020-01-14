583fd4c8cf5e68f4581438fd1c1958bcedfbac76
Paweł Hincka <pawel@hincka.pl>
2020-01-14 17:47:42 +0100

    Added hours loop to chart

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -3,8 +3,29 @@ import { Line } from 'react-chartjs-2';
 
 
 const Chart = (props) => {
+    
+    const getHoursArray = () => {
+        const date = new Date()
+        const currentHour = date.getHours()
+
+        let size = 12
+        let newArray = [currentHour]
+        let now
+
+        while (size >= 0 ) {
+            date.setHours(date.getHours() - newArray.length)
+            now = date.getHours()
+            newArray.push(now)
+            date.setHours(currentHour)
+
+            size--
+        }
+        
+        return newArray.reverse()
+    }   
+
     const data = {
-        labels: props.hr,
+        labels: getHoursArray(),
         datasets: [
             {
                 label: 'Temperatura powietrza',
@@ -21,7 +42,7 @@ const Chart = (props) => {
                 pointHoverBorderWidth: 5,
                 pointRadius: 2,
                 pointHitRadius: 10,
-                data: [12, 13, 22, 28, 20, 15, 10, 7, 5, 8, 10, 3, -0.5]
+                data: [12, 13, 22, 28, 20, 15, 10, 7, 5, 8, 10, 3, -0.5, 10, 3, -0.5]
             }
         ]
     };
--- src/components/weatherInfo.jsx
+++ src/components/weatherInfo.jsx
@@ -45,7 +45,7 @@ class WeatherInfo extends Component{
         )
         const hour = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00']
         console.log(hourData.length)
-        
+
             return (
                 <div className="app__weatherInfo">
                     <header className="weatherInfo__header">
@@ -71,8 +71,7 @@ class WeatherInfo extends Component{
                         </div>
                     </div>
                     <div className="weatherInfo__charts">
-                        <Chart hr={hour}  />
-                        
+                        <Chart hr={hour} />
                     </div>
                 </div>
             );
